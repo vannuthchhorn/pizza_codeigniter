a22ace41728c62d585bcd61aafc0cbce24b32133
vannuth chhorn <vannuthchhron@gmail.com>
2020-07-02 21:23:03 +0700

    config

--- app/Controllers/DashboardPizza.php
+++ app/Controllers/DashboardPizza.php
@@ -79,6 +79,8 @@ class DashboardPizza extends BaseController
 	{
 		$pizzas = new PizzaModel();
 		$data['listPizza'] = $pizzas->find($id);
+
+		
 		return view('index',$data);
 	}
 
@@ -87,6 +89,30 @@ class DashboardPizza extends BaseController
 	{
 		$pizzas = new PizzaModel();
 		$pizzas->update($_POST['id'], $_POST);
+
+		helper(['form']);
+		$data = [];
+		if($this->request->getMethod() == "post")
+		{
+			$rules = [
+				'name'=>'required',
+				'price'=>'required',
+				'ingredients'=>'required'
+			];
+		    if(!$this->validate($rules)){
+				$data['validation'] = $this->validator;
+				return redirect()->to("/dashboard");
+			}else{			
+				$pizzas = new PizzaModel();
+				$pizzaData = array(
+					'name'=>$this->request->getVar('name'),
+					'price'=>$this->request->getVar('price'),
+					'ingredients'=>$this->request->getVar('ingredients'),
+				);
+				$pizzas->addPizza($pizzaData);
+				return redirect()->to("/dashboard");
+			}
+	    }	
 		return redirect()->to('/dashboard');
 	}
 
